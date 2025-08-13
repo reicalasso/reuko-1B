707a61545d26608e041235e939b4f45cd4597527
Rei Calasso <131282394+reicalasso@users.noreply.github.com>
2025-08-13 19:20:46 +0000

    sys.path ayarlaması için gerekli import ifadeleri eklendi

--- scripts/train_sft.py
+++ scripts/train_sft.py
@@ -1,3 +1,7 @@
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../..")))
+
 import yaml
 from datasets import load_dataset
 from transformers import TrainingArguments, Trainer
