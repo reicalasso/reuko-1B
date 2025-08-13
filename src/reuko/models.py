c791c333dd7381bfa40677e298dd1588fe118c56
Rei Calasso <131282394+reicalasso@users.noreply.github.com>
2025-08-13 18:57:30 +0000

    Qwen2.5 modelini ve tokenizer'ını yüklemek için yeni bir fonksiyon eklendi; modelin kullanılabilirliğine göre uygun sınıflar içe aktarıldı.

---
+++ src/reuko/models.py
@@ -0,0 +1,19 @@
+try:
+    from transformers import Qwen2ForCausalLM, Qwen2Tokenizer
+    QWEN2_AVAILABLE = True
+except ImportError:
+    from transformers import AutoModelForCausalLM, AutoTokenizer
+    QWEN2_AVAILABLE = False
+
+def load_qwen_model(model_name, quantize=False):
+    """
+    Qwen2.5 modeli ve tokenizer'ı yükler. trust_remote_code=True ve padding_side ayarlanır.
+    """
+    if QWEN2_AVAILABLE:
+        model = Qwen2ForCausalLM.from_pretrained(model_name, trust_remote_code=True)
+        tokenizer = Qwen2Tokenizer.from_pretrained(model_name, trust_remote_code=True)
+    else:
+        model = AutoModelForCausalLM.from_pretrained(model_name, trust_remote_code=True)
+        tokenizer = AutoTokenizer.from_pretrained(model_name, trust_remote_code=True)
+    tokenizer.padding_side = "left"
+    return model, tokenizer
