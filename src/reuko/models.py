8e7f56e376a4f82caf789599460d71406b9d6d85
Rei Calasso <131282394+reicalasso@users.noreply.github.com>
2025-08-13 19:38:21 +0000

    Qwen2.5 model yükleme fonksiyonunda device_map parametresi eklendi; docstring güncellendi.

--- src/reuko/models.py
+++ src/reuko/models.py
@@ -7,13 +7,13 @@ except ImportError:
 
 def load_qwen_model(model_name, quantize=False):
     """
-    Qwen2.5 modeli ve tokenizer'ı yükler. trust_remote_code=True ve padding_side ayarlanır.
+    Qwen2.5 modeli ve tokenizer'ı yükler. trust_remote_code=True, device_map='cpu' ve padding_side ayarlanır.
     """
     if QWEN2_AVAILABLE:
-        model = Qwen2ForCausalLM.from_pretrained(model_name, trust_remote_code=True)
+        model = Qwen2ForCausalLM.from_pretrained(model_name, trust_remote_code=True, device_map='cpu')
         tokenizer = Qwen2Tokenizer.from_pretrained(model_name, trust_remote_code=True)
     else:
-        model = AutoModelForCausalLM.from_pretrained(model_name, trust_remote_code=True)
+        model = AutoModelForCausalLM.from_pretrained(model_name, trust_remote_code=True, device_map='cpu')
         tokenizer = AutoTokenizer.from_pretrained(model_name, trust_remote_code=True)
     tokenizer.padding_side = "left"
     return model, tokenizer
